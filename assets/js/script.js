ae43280bc693e226f29ea09be87d92387f1120a4
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-05-28 10:49:14 +0000

    Display first question after 'start' click, show buttons, hide question-body.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,45 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
 //Questions
 let questions = [
     {
-        Question: "Do you have children below the age of 25?",
-        Answers: {
+        question: "Do you have children below the age of 25?",
+        answers: {
             a: "yes",
             b: "no",
         }
     },
     {
-        Question: "Are you self-employed?",
-        Answers: {
+        question: "Are you self-employed?",
+        answers: {
             a: "yes",
             b: "no",
         }
     },
     {
-        Question: "Do you own a house or planning to buy one?",
-        Answers: {
+        question: "Do you own a house or planning to buy one?",
+        answers: {
             a: "yes",
             b: "no",
         }
     },
     {
-        Question: "Is the vast majority of your household income coming from 1 person only?",
-        Answers: {
+        question: "Is the vast majority of your household income coming from 1 person only?",
+        answers: {
             a: "yes",
             b: "no",
         }
     },
     {
-        Question: "Does your family members depend mostly on 1 person?",
-        Answers: {
+        question: "Does your family members depend mostly on 1 person?",
+        answers: {
             a: "yes",
             b: "no",
         }
@@ -42,21 +51,29 @@ let username = "";
 
 //Start game when Start is clicked
 function startQuestionnaire() {
-        username = document.getElementById("username").value;
+    username = document.getElementById("username").value;
+    start.classList.add("hide");
+    questionCounter = 0;
+    score = 0;
+    availableQuesions = [...questions];
+    nextQuestion()
 }
 
 //Listen to 'start' clicks
 let start = document.getElementById("start");
 start.addEventListener("click", startQuestionnaire);
+ 
 
+function nextQuestion() {
+    questionCounter++;
+    document.getElementsByTagName("h1")[1].innerText = questions[0].question;
+    document.getElementById("yes").innerText = questions[0].answers.a;
+    document.getElementById("no").innerText = questions[0].answers.b;
+    
 
+    let showButtons = document.getElementById("answers");
+    showButtons.classList.remove("hide");
 
-
-function Questionnaire () {
-
-}
-
-function validateUsername () {
-
-}
-
+    let hide = document.getElementById("question-body");
+    hide.style.display="none";
+};
