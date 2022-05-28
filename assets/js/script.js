efa16516e65f7abb6d47a4f931ab50180d64e36b
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-05-28 09:36:44 +0000

    Add questions array.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,41 @@
-
+//Questions
+let questions = [
+    {
+        Question: "Do you have children below the age of 25?",
+        Answers: {
+            a: "yes",
+            b: "no",
+        }
+    },
+    {
+        Question: "Are you self-employed?",
+        Answers: {
+            a: "yes",
+            b: "no",
+        }
+    },
+    {
+        Question: "Do you own a house or planning to buy one?",
+        Answers: {
+            a: "yes",
+            b: "no",
+        }
+    },
+    {
+        Question: "Is the vast majority of your household income coming from 1 person only?",
+        Answers: {
+            a: "yes",
+            b: "no",
+        }
+    },
+    {
+        Question: "Does your family members depend mostly on 1 person?",
+        Answers: {
+            a: "yes",
+            b: "no",
+        }
+    },
+ ]
 
 // Store username in a variable
 let username = "";
