5bf9e66c72eda7aea44bbec10ff94a12e36c8c38
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-05-30 10:44:52 +0000

    Remove validateInput as superflual.

--- assets/js/life-insurance-calculator.js
+++ assets/js/life-insurance-calculator.js
@@ -2,17 +2,13 @@ let salary = 0;
 let years = 0;
 let result = 0;
 
-function validateInput() {
+function calculate() {
     salary = document.getElementById("salary").value;
     years = document.getElementById("years").value;
-    calculate();
-}
-
-function calculate() {
     result = salary * years;
-    document.getElementById("life-cover-result").innerText = result;
+    document.getElementById("life-cover-result").innerText = result + `€ is the amount of cover you need!`;
 }
 
 // Listen to 'calculate' clicks
 let calculateBtn = document.getElementById("calculate-btn");
-calculateBtn.addEventListener("click", validateInput);
+calculateBtn.addEventListener("click", calculate);
