d3dae5b7bbac5d3870ea4eb2ccb21cff2375a8d6
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-05-30 09:58:28 +0000

    Add event listener for calculate button.

--- assets/js/life-insurance-calculator.js
+++ assets/js/life-insurance-calculator.js
@@ -2,10 +2,13 @@ let salary = document.getElementById("salary").value;
 let years = document.getElementById("years").value;
 
 function validateInput() {
-
+    alert('works');
 }
 
 function calculate() {
 
 }
 
+// Listen to 'calculate' clicks
+let calculateBtn = document.getElementById("calculate-btn");
+calculateBtn.addEventListener("click", validateInput);
